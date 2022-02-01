438d9d7a6620e4c36fa5186bbe3ed11824b06aa2
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2022-02-01 13:58:21 -0500

    impliment character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $(".form-text").on('input', function() {
+    let input = $(this)
+    let length = input.val().length;
+    console.log('length', length);
+  
+    let counter = $('#counter');
+    counter.val(140 - length);
+    if(counter.val() < 0) {
+      counter.css('color', 'red');
+    }else{
+      counter.css('color', 'black');
+    }
+  })
+});
